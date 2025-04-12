fb3ceb627234926028bd0fbf4ab316a0330d369f
cihankarimli <kerimlicihan1@gmail.com>
2025-04-12 14:02:41 +0400

    deyisdirildi

--- src/componets/ToDoModul.jsx
+++ src/componets/ToDoModul.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-function ToDoModul({ openModul, closeModul }) {
+function ToDoModul({ openModul, closeModul, todo }) {
+  const { content } = todo;
   return (
     <>
       {openModul && (
         <div className="modul">
           <div className="modul-border">
             <h2>Modul</h2>
-            <p>{openModul.content}</p>
+            <p> {content}</p>
             <button className="modul-close" onClick={closeModul}>
               close
             </button>
